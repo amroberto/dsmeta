2a31b0d3a57c58b2b1ca647e8d064f8102af89cc
Alexandre Martins Roberto <amroberto@gmail.com>
2022-09-09 17:50:06 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -22,8 +22,6 @@ const [sales, setSales] = useState<Sale[]>([]);
       const dmin = minDate.toISOString().slice(0, 10);
       const dmax = maxDate.toISOString().slice(0, 10);
       
-      console.log(dmin);
-      
       axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
         .then(response => {
           setSales(response.data.content);
@@ -77,7 +75,7 @@ const [sales, setSales] = useState<Sale[]>([]);
                           <td>R$ {sale.amount.toFixed(2)}</td>
                           <td>
                             <div className="dsmeta-red-btn-container">
-                              <NottificationButton />
+                              <NottificationButton saleId={sale.id}/>
                             </div>
                           </td>
                         </tr>
